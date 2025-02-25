2b9aeae0e4820a2d3a1c4616315aa35c991c50c3
Ismail-Lafhiel <odashimidorida@gmail.com>
2025-02-25 14:56:07 +0000

    NOM-29 installed tailwind

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import "./global.css"
+
+import React from 'react';
+import Test from "./src/components/Test";
+
+function App(): React.JSX.Element {
+
+
+  return (
+    <Test />
+  );
+}
+
+export default App;
